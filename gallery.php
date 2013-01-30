ee9455b27a583f0f85047a02eee8a319086887d6
DaanVos <me@daanvosdewael.com>
2013-01-30 01:13:55 +0100

    init commit

---
+++ gallery.php
@@ -0,0 +1,71 @@
+<?php
+/*
+ * @author    Daan Vos de Wael
+ * @copyright Copyright (c) 2013, Daan Vos de Wael, http://www.daanvosdewael.com
+ * @license   http://en.wikipedia.org/wiki/MIT_License The MIT License
+*/
+
+  function gallery_metabox_enqueue($hook) {
+    if ( 'post.php' == $hook || 'post-new.php' == $hook ) {
+      wp_enqueue_script('gallery-metabox', get_template_directory_uri() . '/custom-meta-boxes/js/gallery-metabox.js', array('jquery', 'jquery-ui-sortable'));
+      wp_enqueue_style('gallery-metabox', get_template_directory_uri() . '/custom-meta-boxes/css/gallery-metabox.css');
+    }
+  }
+  add_action('admin_enqueue_scripts', 'gallery_metabox_enqueue');
+
+  function add_gallery_metabox() {
+    add_meta_box(
+      'gallery-metabox',
+      'Gallery',
+      'gallery_meta_callback',
+      'post',
+      'normal',
+      'high'
+    );
+  }
+  add_action('add_meta_boxes', 'add_gallery_metabox');
+
+  function gallery_meta_callback($post) {
+    wp_nonce_field( basename(__FILE__), 'gallery_meta_nonce' );
+    $ids = get_post_meta($post->ID, 'vdw_gallery_id', true);
+
+    ?>
+    <table class="form-table">
+      <tr><td>
+        <a class="gallery-add button" href="#" data-uploader-title="Add image(s) to gallery" data-uploader-button-text="Add image(s)">Add image(s)</a>
+
+        <ul id="gallery-metabox-list">
+        <?php if ($ids) :
+        foreach ($ids as $key => $value) :
+          $image = wp_get_attachment_image_src($value); ?>
+
+          <li>
+            <input type="hidden" name="vdw_gallery_id[<?php echo $key; ?>]" value="<?php echo $value; ?>">
+            <img class="image-preview" src="<?php echo $image[0]; ?>">
+            <a class="change-image button button-small" href="#" data-uploader-title="Change image" data-uploader-button-text="Change image">Change image</a><br>
+            <small><a class="remove-image" href="#">Remove image</a></small>
+          </li>
+
+        <?php endforeach; endif; ?>
+        </ul>
+
+      </td></tr>
+    </table>
+  <?php }
+
+  function gallery_meta_save($post_id) {
+    if (!isset($_POST['gallery_meta_nonce']) || !wp_verify_nonce($_POST['gallery_meta_nonce'], basename(__FILE__))) return;
+
+    if (!current_user_can('edit_post', $post_id)) return;
+
+    if (defined('DOING_AUTOSAVE') && DOING_AUTOSAVE) return;
+
+    if(isset($_POST['vdw_gallery_id'])) {
+      update_post_meta($post_id, 'vdw_gallery_id', $_POST['vdw_gallery_id']);
+    } else {
+      delete_post_meta($post_id, 'vdw_gallery_id');
+    }
+  }
+  add_action('save_post', 'gallery_meta_save');
+
+?>
---
+++ js/gallery-metabox.js
@@ -0,0 +1,94 @@
+jQuery(function($) {
+
+  var file_frame;
+
+  $(document).on('click', '#gallery-metabox a.gallery-add', function(e) {
+
+    e.preventDefault();
+
+    if (file_frame) {
+      file_frame.close();
+    }
+
+    file_frame = wp.media.frames.file_frame = wp.media({
+      title: $(this).data('uploader-title'),
+      button: {
+        text: $(this).data('uploader-button-text'),
+      },
+      multiple: true
+    });
+
+    file_frame.on('select', function() {
+      var listIndex = $('#gallery-metabox-list li').index($('#gallery-metabox-list li:last')),
+          selection = file_frame.state().get('selection');
+
+      selection.map(function(attachment, i) {
+        attachment = attachment.toJSON(),
+        index      = listIndex + (i + 1);
+
+        $('#gallery-metabox-list').append('<li><input type="hidden" name="vdw_gallery_id[' + index + ']" value="' + attachment.id + '"><img class="image-preview" src="' + attachment.sizes.thumbnail.url + '"><a class="change-image button button-small" href="#" data-uploader-title="Change image" data-uploader-button-text="Change image">Change image</a><br><small><a class="remove-image" href="#">Remove image</a></small></li>');
+      });
+    });
+
+    makeSortable();
+    
+    file_frame.open();
+
+  });
+
+  $(document).on('click', '#gallery-metabox a.change-image', function(e) {
+
+    e.preventDefault();
+
+    var that = $(this);
+
+    if (file_frame) {
+      file_frame.close();
+    }
+
+    file_frame = wp.media.frames.file_frame = wp.media({
+      title: $(this).data('uploader-title'),
+      button: {
+        text: $(this).data('uploader-button-text'),
+      },
+      multiple: false
+    });
+
+    file_frame.on( 'select', function() {
+      attachment = file_frame.state().get('selection').first().toJSON();
+
+      that.parent().find('input:hidden').attr('value', attachment.id);
+      that.parent().find('img.image-preview').attr('src', attachment.sizes.thumbnail.url);
+    });
+
+    file_frame.open();
+
+  });
+
+  function resetIndex() {
+    $('#gallery-metabox-list li').each(function(i) {
+      $(this).find('input:hidden').attr('name', 'vdw_gallery_id[' + i + ']');
+    });
+  }
+
+  function makeSortable() {
+    $('#gallery-metabox-list').sortable({
+      opacity: 0.6,
+      stop: function() {
+        resetIndex();
+      }
+    });
+  }
+
+  $(document).on('click', '#gallery-metabox a.remove-image', function(e) {
+    e.preventDefault();
+
+    $(this).parents('li').animate({ opacity: 0 }, 200, function() {
+      $(this).remove();
+      resetIndex();
+    });
+  });
+
+  makeSortable();
+
+});
